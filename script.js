2485ebd57cf60a39429df63a5e802553ec17be96
Mirella Sawer <mirella.sawer.martins@escola.pr.gov.br>
2024-11-14 11:34:55 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const avancarBtns = document.querySelectorAll('.btn-proximo');
+
+avancarBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        const passoAtual = document.querySelector('.ativo');
+        const idProximoPasso = `passo-${this.dataset.proximo}`;
+
+        passoAtual.classList.remove('ativo');
+        document.getElementById(idProximoPasso).classList.add('ativo');
+    });
+});
